ea540e3a42209c94bd687367f1e9a698877740b5
Philipp Schüle <p.schuele@metaways.de>
2023-09-19 11:23:28 +0200

    tweak(console/TineCommand): add info output to mailstack commands

--- cli/Commands/Tine/TineCommand.php
+++ cli/Commands/Tine/TineCommand.php
@@ -6,7 +6,10 @@ use App\Commands\Docker\DockerCommand;
 
 class TineCommand extends DockerCommand
 {
-    public function mailstackInit($io) {
+    public function mailstackInit($io)
+    {
+        $io->info("Initializing mailstack ...");
+
         $out = system($this->getComposeString() . ' run --rm mailstack init');
 
         if ('' == $out) {
@@ -16,7 +19,10 @@ class TineCommand extends DockerCommand
         return 1;
     }
 
-    public function mailstackReset($io) {
+    public function mailstackReset($io)
+    {
+        $io->info("Resetting mailstack ...");
+
         $out = system($this->getComposeString() . ' run --rm mailstack reset');
 
         if ('' == $out) {
@@ -26,15 +32,19 @@ class TineCommand extends DockerCommand
         return 1;
     }
 
-    public function setupCli($cmd) {
-        passthru($this->getComposeString() . ' exec --user tine20 web sh -c "cd /usr/share/tine20/ && php setup.php ' . $cmd . '"', $result_code);
+    public function setupCli($cmd)
+    {
+        passthru($this->getComposeString()
+            . ' exec --user tine20 web sh -c "cd /usr/share/tine20/ && php setup.php ' . $cmd . '"', $result_code);
 
         return $result_code;
     }
 
-    public function tineCli($cmd) {
+    public function tineCli($cmd)
+    {
         //--config \$TINE20_CONFIG_PATH
-        passthru($this->getComposeString() . ' exec --user tine20 web sh -c "cd /usr/share/tine20/ && php tine20.php ' . $cmd . '"', $result_code);
+        passthru($this->getComposeString()
+            . ' exec --user tine20 web sh -c "cd /usr/share/tine20/ && php tine20.php ' . $cmd . '"', $result_code);
 
         return $result_code;
     }
