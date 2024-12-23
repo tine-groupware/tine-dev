735cd4ec9019495ccf8ba4bbeebd927e62ce4ea8
Paul Mehrer <p.mehrer@metaways.de>
2024-12-23 22:59:25 +0100

    added edocument view svc url, update edocument service version

--- configs/edocument-services/edocument-services.inc.php
+++ configs/edocument-services/edocument-services.inc.php
@@ -3,7 +3,8 @@
 return [
     'Sales' => [
         'edocument' => [
-            'validation_svc' => 'http://edocument-services:3000/ubl'
+            'validation_svc' => 'http://edocument-services:3000/ubl',
+            'view_svc' => 'http://edocument-services:3000/ublView',
         ]
     ]
 ];
