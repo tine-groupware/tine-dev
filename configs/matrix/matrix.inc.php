b132c123b31b477179b60fd68b5a9a9cb6801881
Milan Mertens <m.mertens@metaways.de>
2025-03-25 10:38:46 +0100

    adds matrix synapse

---
+++ configs/matrix/matrix.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'MatrixSynapseIntegrator' => [
+        'matrixDomain' => 'matrix.locale.tine-dev.de',
+        'homeServerUrl' => 'https://matrix.locale.tine-dev.de'
+    ],
+];
