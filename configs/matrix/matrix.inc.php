d22cad0fb40ca2dd6865aeb7ff483b7c0358702e
Milan Mertens <m.mertens@metaways.de>
2025-10-13 14:37:05 +0200

    matrix: configure shared secret authenticator

--- configs/matrix/matrix.inc.php
+++ configs/matrix/matrix.inc.php
@@ -3,6 +3,9 @@
 return [
     'MatrixSynapseIntegrator' => [
         'matrixDomain' => 'matrix.local.tine-dev.de',
-        'homeServerUrl' => 'https://matrix.local.tine-dev.de'
+        'homeServerUrl' => 'https://matrix.local.tine-dev.de',
+        'elementUrl' => 'http://localhost:8080',
+        'matrixSynapseSharedSecretAuth' => false,
+        'corporalSharedAuthToken' => 'SynapseSharedSecretAuthenticatorTineSharedSecret',
     ],
 ];
